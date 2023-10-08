5c4ea71289cfa72c5c31e49c6e77c45bd4b17dc5
Adem Han <adem.han@outlook.com>
2023-10-08 13:54:32 +0300

    Sahneler peş peşe eklendi. Seslendirmeler dahil edildi.

---
+++ Assets/Crystal_Mine/SahneGecisButton.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SahneGecisButton : MonoBehaviour
+{
+    public void Hikaye1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Hikaye2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Hikaye3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Hikaye4()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void Hikaye5()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Hikaye6()
+    {
+        SceneManager.LoadScene(6);
+    }
+
+    public void Hikaye7()
+    {
+        SceneManager.LoadScene(7);
+    }
+
+    public void Hikaye8()
+    {
+        SceneManager.LoadScene(8);
+    }
+
+    public void Hikaye9()
+    {
+        SceneManager.LoadScene(9);
+    }
+
+    public void OyunaGiris()
+    {
+        SceneManager.LoadScene(10);
+    }
+
+
+    public void credits()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void anamenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
